d929c7f5792323cb5a88939683d6ef262e5ac2c2
origami <1071242996@qq.com>
2022-09-28 13:46:28 +0800

    函数组件模拟生命周期

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react";
+
+const App = props => {
+    const [childVisible, setChildVisible] = useState(true)
+    const hide = () => {
+        setChildVisible(false)
+    }
+    const show = () => {
+        setChildVisible(true)
+    }
+    return (
+        <div>
+            {childVisible ? <button onClick={hide}>hide</button> : <button onClick={show}>show</button>}
+            {childVisible ? <Child/> : null}
+        </div>
+    )
+}
+
+const Child = (props) => {
+    useEffect(()=>{
+        console.log('渲染变化了')
+        return ()=>{
+            console.log('Child销毁了')
+        }
+    })
+    return (
+        <div>Child</div>
+    )
+}
+
+export default App;
